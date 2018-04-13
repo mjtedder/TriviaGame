a025818a1e994d7d88278fcbd05dd930dc8981e7
unknown <mjtedder@gmail.com>
2018-04-13 14:50:03 -0400

    logic working, added basic styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,124 +1,138 @@
+//todo: add images after each question is correctly or incorrectly answered
+//todo: randomize questions
+
+//Method that allows extending jQuery with own functions
 $.fn.trivia = function() {
-    var _t = this;
-    _t.userPick = null;
-    _t.answers = {
-      correct: 0,
-      incorrect: 0
-    };
-    _t.images = null;
-    _t.count = 30;
-    _t.current = 0;
-    _t.questions = [{
-        question: "What city does the show Breaking Bad take place?",
-        choices: ["Des Moines", "Salt Lake City", "Albuquerque", "Denver"],
-        images: ["../images/nm-albuquerque.gif"],
-        correct: 2
-      }, {
-        question: "What is the name of the first buyer Walter White meets?",
-        choices: ["Pancakez", "Big Boi", "Krazy 8", "John Smith"],
-        images: ["../images/krazy_8.jpg"],
-        correct: 2
-      }, {
-        question: "What was the name of the plant that Walt used to poison Brock",
-        choices: ["Lily of the Valley", "Yellow Jessamine", "Palma Christi"],
-        correct: 0
-      }, {
-        question: "What is Walt's main car in the series?",
-        choices: ["1986 Toyota Tercel", "2004 Pontiac Aztec", "2005 Mazda 3", "2006 Chrysler PT Cruiser"],
-        correct: 1
-      }, {
-        question: "What is the street name for the ultra-pure product that Walt manufacturers?",
-        choices: ["Crystal", "Crank", "Ice", "Blue", "Meth"],
-        correct: 3
-      }, {
-        question: "What is the occupation of Jesse's girlfriend in season 2 Jane's father?",
-        choices: ["Pilot", "Bus Driver", "Police Officer", "Air Traffic Controller"],
-        correct: 3
-      }, {
-        question: "What is the address of the White family?",
-        choices: ["290 Wrong Way", "357 Main Street", "400 Azul Blanco Road", "308 Negra Arroya Lane"],
-        correct: 3
-      }];
-      _t.ask = function() {
-          if (_t.questions[_t.current]) {
-              $("#timer").html("Time remaining: " + "00:" + _t.count + " secs");
-              $("#question_div").html(_t.questions[_t.current].question);
-              var choicesArr = _t.questions[_t.current].choices;
-              var buttonsArr = [];
+var game = this;
+game.userPick = null;
+game.answers = {
+  correct: 0,
+  incorrect: 0
+};
+game.images = null;
+game.count = 30;
+game.current = 0;
+game.questions = [{
+  question: "What city does the show Breaking Bad take place?",
+  choices: ["Des Moines", "Charlotte", "Albuquerque", "Denver"],
+  correct: 2
+}, {
+  question: "What is the name of the first buyer Walter White meets?",
+  choices: ["Pancakez", "Junior", "Krazy 8", "Scarface"],
+  correct: 2,
+}, {
+  question: "What was the name of the plant that Walt used to poison Brock",
+  choices: ["Lily of the Valley", "Yellow Jessamine", "Palma Christi", "Sunflower"],
+  correct: 0,
+}, {
+  question: "What is Walt's main car in the series?",
+  choices: ["1986 Toyota Tercel", "2004 Pontiac Aztec", "2005 Mazda 3", "2006 Chrysler PT Cruiser"],
+  correct: 1,
+}, {
+  question: "What is the street name for the ultra-pure product that Walt manufacturers?",
+  choices: ["Crystal", "Crank", "Ice", "Blue"],
+  correct: 3,
+}, {
+  question: "In Season 2, what is the occupation of Jesse's girlfriend Jane's father?",
+  choices: ["Pilot", "Bus Driver", "Police Officer", "Air Traffic Controller"],
+  correct: 3,
+}, {
+  question: "What is the address of the White family?",
+  choices: ["290 Wrong Way", "357 Main Street", "400 Azul Blanco Road", "308 Negra Arroya Lane"],
+  correct: 3,
+}, {
+  question: "What is the name of the crooked lawyer Walt & Jesse employs when one of their dealers are arrested?",
+  choices: ["Drew Scales", "Phoenix Wright", "Howard Hamlin", "Saul Goodman"],
+  correct: 3,
+}];
 
-              for (var i = 0; i < choicesArr.length; i++) {
-                  var button = $('<button>');
-                  button.text(choicesArr[i]);
-                  button.attr('data-id', i);
-                  $('#choices_div').append(button);
-              }
-              window.triviaCounter = setInterval(_t.timer, 1000);
-          } else {
-              $('body').append($('<div />', {
-                  text: 'Unanswered: ' + (
-                      _t.questions.length - (_t.answers.correct + _t.answers.incorrect)),
-                  class: 'result'
-              }));
-              $('#start_button').text('Restart').appendTo('body').show();
-          }
-      };
-      _t.timer = function() {
-          _t.count--;
-          if (_t.count <= 0) {
-              setTimeout(function() {
-                  _t.nextQ();
-              });
-
-          } else {
-              $("#timer").html("Time remaining: " + "00:" + _t.count + " secs");
-          }
-      };
-      _t.nextQ = function() {
-          _t.current++;
-          clearInterval(window.triviaCounter);
-          _t.count = 30;
-          $('#timer').html("");
-          setTimeout(function() {
-              _t.cleanUp();
-              _t.ask();
-          }, 1000)
-      };
-      _t.cleanUp = function() {
-          $('div[id]').each(function(item) {
-              $(this).html('');
-          });
-          $('.correct').html('Correct answers: ' + _t.answers.correct);
-          $('.incorrect').html('Incorrect answers: ' + _t.answers.incorrect);
-      };
-      _t.answer = function(correct) {
-          var string = correct ? 'correct' : 'incorrect';
-          _t.answers[string]++;
-          $('.' + string).html(string + ' answers: ' + _t.answers[string]);
-      };
-      return _t;
-  };
-  var Trivia;
-
-  $("#start_button").click(function() {
-      $(this).hide();
-      $('.result').remove();
-      $('div').html('');
-      Trivia = new $(window).trivia();
-      Trivia.ask();
+//Function for asking questions in game.questions array
+game.ask = function() {
+  if (game.questions[game.current]) {
+    $("#timer").html("Time remaining: " + "00:" + game.count + " secs");
+    $("#question_div").html(game.questions[game.current].question);
+    var choicesArr = game.questions[game.current].choices;
+    var buttonsArr = [];
+    //Looping through choices in the array and appending to a button element
+    for (var i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    }
+    window.triviaCounter = setInterval(game.timer, 1000);
+  } else {
+    $('body').append($('<div />', {
+      text: 'Unanswered: ' + (
+        game.questions.length - (game.answers.correct + game.answers.incorrect)),
+      class: 'result'
+    }));
+    $('#start_button').text('Restart').appendTo('body').show();
+  }
+};
+//Function for timer
+game.timer = function() {
+  game.count--;
+  if (game.count <= 0) {
+    setTimeout(function() {
+      game.nextQ();
+    });
+    //Appending time remaining to a div in html
+  } else {
+    $("#timer").html("Time remaining: " + "00:" + game.count + " secs");
+  }
+};
+//Function for switching to next question in array
+game.nextQ = function() {
+  game.current++;
+  clearInterval(window.triviaCounter);
+  game.count = 30;
+  $('#timer').html("");
+  setTimeout(function() {
+    game.cleanUp();
+    game.ask();
+  }, 1000)
+};
+//Removes previous question, displays score, moves onto next questions
+game.cleanUp = function() {
+  $('div[id]').each(function(item) {
+    $(this).html('');
   });
+  //Displays number of correct and incorrect answers
+  $('.correct').html('Correct answers: ' + game.answers.correct);
+  $('.incorrect').html('Incorrect answers: ' + game.answers.incorrect);
+};
+game.answer = function(correct) {
+  var string = correct ? 'correct' : 'incorrect';
+  game.answers[string]++;
+  $('.' + string).html(string + ' answers: ' + game.answers[string]);
+};
+return game;
+};
+var Trivia;
 
-  $('#choices_div').on('click', 'button', function(e) {
-      var userPick = $(this).data("id"),
-          _t = Trivia || $(window).trivia(),
-          index = _t.questions[_t.current].correct,
-          correct = _t.questions[_t.current].choices[index];
+$("#start_button").click(function() {
+  $(this).hide();
+  $('.result').remove();
+  $('div').html('');
+  Trivia = new $(window).trivia();
+  Trivia.ask();
+});
 
-      if (userPick !== index) {
-          $('#choices_div').text("Wrong Answer! The correct answer was: " + correct);
-          _t.answer(false);
-      } else {
-          $('#choices_div').text("Correct!!! The correct answer was: " + correct);
-          _t.answer(true);
-      }
-      _t.nextQ();
-  });
+//Function for when user clicks a choice button..
+$('#choices_div').on('click', 'button', function() {
+  var userPick = $(this).data("id"),
+    game = Trivia || $(window).trivia(),
+    index = game.questions[game.current].correct,
+    correct = game.questions[game.current].choices[index];
+
+//Conditional statement deciding what happens if choice is correct or incorrect
+  if (userPick !== index) {
+    $('#choices_div').text("Not even close!!! The correct answer was: " + correct);
+    game.answer(false);
+  } else {
+    $('#choices_div').text("You're goddamn right!!! The correct answer was: " + correct);
+    game.answer(true);
+  }
+  game.nextQ();
+});
